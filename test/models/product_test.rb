7a47e09197d86b982afb865edbd66ac8e8be4854
Sandro Queiroz Jr <sandroqz@gmail.com>
2018-03-31 17:24:33 -0300

    Playtime
    Add validation to the Product model to check that the title is at least ten characters;
    Change the error message associated with the product price.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -16,11 +16,11 @@ class ProductTest < ActiveSupport::TestCase
     product = Product.new(title: "My Book Title", description: "yyy", image_url: "zzz.jpg")
     product.price = -1
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    assert_equal ["must be greater than or equal to one cent"], product.errors[:price]
 
     product.price = 0
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    assert_equal ["must be greater than or equal to one cent"], product.errors[:price]
 
     product.price = 1
     assert product.valid?
@@ -55,5 +55,12 @@ class ProductTest < ActiveSupport::TestCase
 
     assert product.invalid?
     assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
+
+  test "product title is at least ten characters long" do
+    product = Product.new(title: "too short", description: "yyy", price: 1, image_url: "fred.gif")
+
+    assert product.invalid?
+    assert_equal ["is too short (minimum is 10 characters)"], product.errors[:title]
   end
 end
